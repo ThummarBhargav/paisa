ed6a670b6fd953741b8efe950f8a9b5fd51b3e17
Bhargav Thummar <thummar0211@gmail.com>
2023-10-19 12:12:21 +0530

    one signal

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:paisa/app.dart';
 import 'package:paisa/core/enum/box_types.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:paisa/di/di.dart';
 import 'package:paisa/features/recurring/domain/repository/recurring_repository.dart';
 
@@ -25,6 +26,16 @@ Future<void> main() async {
 
   await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.setAppId("a56b8f66-9401-4683-a057-d898aa474783");
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
+  OneSignal.shared.setNotificationWillShowInForegroundHandler(
+      (OSNotificationReceivedEvent event) {
+    print(event.notification.body);
+    event.complete(event.notification);
+  });
   await MobileAds.instance.initialize();
   await MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
       testDeviceIds: ["4796378E2BBA42CC7D1DE7367E50828B"]));
